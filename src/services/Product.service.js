38b5fad531a6e955d8089029802076eafabe1bfc
Alex Lino <alejandrolinojaraquemada@gmail.com>
2022-12-07 20:12:54 +0100

    owner

--- src/services/Product.service.js
+++ src/services/Product.service.js
@@ -7,6 +7,17 @@ class ProductService {
         this.api = axios.create({
             baseURL: `${process.env.REACT_APP_API_URL}/products`
         })
+
+        this.api.interceptors.request.use((config) => {
+
+            const storedToken = localStorage.getItem("authToken");
+
+            if (storedToken) {
+                config.headers = { Authorization: `Bearer ${storedToken}` }
+            }
+
+            return config
+        })
     }
 
     getOneProduct(product_id) {
